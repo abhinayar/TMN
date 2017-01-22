024af10737d309cfad55998ae46a5ff582afae06
Abhi Nayar <abhinayar@Abhis-Air.wireless.yale.internal>
2017-01-21 19:46:58 -0500

    Fixes

--- public/js/main.js
+++ public/js/main.js
@@ -69,7 +69,6 @@ $(document).ready(function(){
 	
 	$(".solution-content, .subpage-prod-content").click(function(){
 		var tget = $(this).attr("data-target");
-		alert(tget);
 		if (tget === "Consumers") {
 			window.location.href= "http://www.jumpnet.com";
 		} else {
@@ -81,7 +80,7 @@ $(document).ready(function(){
 				subFolder = "Products/"
 			}
 			var curr_url = window.location.href.toString();
-			var new_url = curr_url.substring(0, curr_url.indexOf("io/") + 1) + subFolder + tget + ".html";
+			var new_url = curr_url.substring(0, curr_url.indexOf("io/") + 3) + subFolder + tget + ".html";
 			console.log(new_url);
 			window.location.href = new_url;
 		}
